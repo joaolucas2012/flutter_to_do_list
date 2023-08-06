b8fc3b6ec97017bb62ea7730cef96cc5a4d3748c
@joaolucas-prime <jlucas@grupo-articom.com>
2023-08-06 12:35:38 -0300

    :rocket: [ADD] create project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_list/pages/to_do_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TodoListListPage(),
+    );
+  }
+}
---
+++ lib/pages/to_do_list_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TodoListListPage extends StatelessWidget {
+  const TodoListListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
