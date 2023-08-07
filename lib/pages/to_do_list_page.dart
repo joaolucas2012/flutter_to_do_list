c0e3d075a65e45237483726e13a76ba84ab87589
@joaolucas-prime <jlucas@grupo-articom.com>
2023-08-07 08:49:02 -0300

    :bulb: [ADD] implemented an input text with two buttons (one for adding a task and another for clean all the tasks)

--- lib/pages/to_do_list_page.dart
+++ lib/pages/to_do_list_page.dart
@@ -5,8 +5,67 @@ class TodoListListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Add a task',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          print("Clicou");
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          fixedSize: const Size(20, 45),
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          size: 20,
+                        )),
+                  )
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text(
+                      'You have 0 pending tasks',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("Clicou");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        fixedSize: const Size(125, 35),
+                      ),
+                      child: const Text('Clean all tasks'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
